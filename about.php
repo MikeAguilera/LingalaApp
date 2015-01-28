e26d08f2e067457e343a69585eec5dbc258a9391
MikeAguilera <michael.aguilera@gmail.com>
2015-01-27 19:22:35 -0800

    updated pages to php added json

---
+++ about.php
@@ -0,0 +1,55 @@
+<!--Header Begin-->
+<?php 
+$pageTitle = "About Us";
+include 'inc/header.php'; ?>
+<!--Header End-->
+
+
+<div id="page-wrapper">
+
+    <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome <small> to Learn Lingala!</small>
+                        </h1>
+                        <ol class="breadcrumb">
+                            <li class="active">
+                                <i class="fa fa-home"></i> Home
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+<div class="jumbotron">
+    <h1>About Us</h1>
+    <p>Am am social worker and tech consultant. I have my MSW and also self taught computer programming.</p>
+</div>
+
+</div>
+</div>
+
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="js/plugins/morris/raphael.min.js"></script>
+    <script src="js/plugins/morris/morris.min.js"></script>
+    <script src="js/plugins/morris/morris-data.js"></script>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/slickQuiz-config.js"></script>
+    <script src="js/slickQuiz.js"></script>
+    <script src="js/master.js"></script>
+    
+    </body>
+
+
+</html>
