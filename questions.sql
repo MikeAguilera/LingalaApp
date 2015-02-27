ad6018488ef1c763d431a6ca979f2a40b789acf8
MikeAguilera <michael.aguilera@gmail.com>
2015-02-27 11:59:14 -0800

    added questions and answeres
    added questions and answers to the SQL

--- questions.sql
+++ questions.sql
@@ -8,32 +8,9 @@ CREATE TABLE IF NOT EXISTS `basic_1` (
   `OptD` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-INSERT INTO `basic_1` (`Question`, `OptA`, 'OptB', 'OptC', 'OptD') VALUES('What is the Lingala Subject Marker for "I"?', 'Na','O','A','E');
-INSERT INTO `basic_1` (`Question`, `OptA`, 'OptB', 'OptC', 'OptD') VALUES('What is the Lingala Subject Marker for "You"?', 'O','Ba','To','A');
+INSERT INTO `basic_1` (`Question`, `OptA`, 'OptB', 'OptC', 'OptD') VALUES('What is the Lingala Subject Marker for \"I\"?', 'Na','O','A','E');
+INSERT INTO `basic_1` (`Question`, `OptA`, 'OptB', 'OptC', 'OptD') VALUES('What is the Lingala Subject Marker for \"You\"?', 'O','Ba','To','A');
 INSERT INTO `basic_1` (`Question`, `OptA`, 'OptB', 'OptC', 'OptD') VALUES('What is the Lingala Subject Marker for "He\\She"?', 'A','E','O','Bo');
-INSERT INTO `basic_1` (`Question`, `OptA`, 'OptB', 'OptC', 'OptD') VALUES('What is the Lingala Subject Marker for "It"?', 'E','To','A','O');
-INSERT INTO `basic_1` (`Question`, `OptA`, 'OptB', 'OptC', 'OptD') VALUES('What is the Lingala Subject Marker for "We"?', 'To','Bo','E','Ba');
-INSERT INTO `basic_1` (`Question`, `OptA`, 'OptB', 'OptC', 'OptD') VALUES('What is the Lingala Subject Marker for "They"?', 'Ba','A','O','Bo')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+INSERT INTO `basic_1` (`Question`, `OptA`, 'OptB', 'OptC', 'OptD') VALUES('What is the Lingala Subject Marker for \"It\"?', 'E','To','A','O');
+INSERT INTO `basic_1` (`Question`, `OptA`, 'OptB', 'OptC', 'OptD') VALUES('What is the Lingala Subject Marker for \"We\"?', 'To','Bo','E','Ba');
+INSERT INTO `basic_1` (`Question`, `OptA`, 'OptB', 'OptC', 'OptD') VALUES('What is the Lingala Subject Marker for \They\"?', 'Ba','A','O','Bo')
